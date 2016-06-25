629d14711cb7fb995726fbc5481a7dd68f0f6a89
Brandon Allen <firexfighterx@gmail.com>
2016-06-25 12:44:16 -0500

    Got routing working, need to get nav bar working with routing

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var hashHistory = ReactRouter.hashHistory;
+var About = require('./components/about/about');
+var Home = require('./components/home');
+
+var routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/home" component={Home}/>
+    <Route path="/about" component={About}/>
+  </Router>
+);
+
+module.exports = routes;
