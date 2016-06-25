04f4f51e44ba465e7b853df3bcafff9ce723cf31
Brandon Allen <firexfighterx@gmail.com>
2016-06-24 19:08:36 -0500

    Added an about component with sudo routing enabled

---
+++ src/components/about/about.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+  render: function(){
+    return (
+      <div>About my funckin site</div>
+    );
+  }
+});
+
+module.exports = About;
--- src/main.js
+++ src/main.js
@@ -2,5 +2,28 @@ $ = jQuery = require('jQuery');
 var React = require('react');
 var reactDom = require('react-dom');
 var Home = require('./components/home');
+var About = require('./components/about/about');
 
-reactDom.render(<Home />, document.getElementById('app'));
+var App = React.createClass({
+  render: function(){
+    var Child;
+
+    switch(this.props.route)
+    {
+      case '/about': Child = About; break;
+      default: Child = Home;
+    }
+
+    return (
+      <div><Child /></div>
+    );
+  }
+});
+
+function render(){
+  var route = window.location.hash.substr(1);
+  reactDom.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('haschange', render);
+render();
