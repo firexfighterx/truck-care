d8b997a0961192641484506f5e2594265abe2214
Brandon Allen <firexfighterx@gmail.com>
2016-09-05 12:59:20 -0500

    Adding tests for the store when dispatching a load truck success

--- src/tests/store/storeTests.js
+++ src/tests/store/storeTests.js
@@ -1,10 +1,12 @@
 import expect from 'expect';
+import assert from 'assert';
 import {
     createStore
 } from 'redux';
 import rootReducer from '../../reducers';
 import initialState from '../../reducers/initialState';
 import * as courseActions from '../../actions/courseActions';
+import * as truckCareActions from '../../actions/TruckCareActions';
 
 describe('Store', function() {
     it('Should handle creating courses', function() {
@@ -25,5 +27,19 @@ describe('Store', function() {
         };
 
         expect(actual).toEqual(expected);
+    });
+
+    it('should update store with dispatched trucks', () => {
+        const store = createStore(rootReducer, initialState);
+        let trucks = [{
+            id: 1,
+            truckName: '2400'
+        }];
+
+        let action = truckCareActions.loadTrucksSuccess(trucks);
+        store.dispatch(action);
+
+        let actual = store.getState().trucks;
+        assert.deepEqual(actual, trucks, 'trucks was set on the state store');
     });
 });
