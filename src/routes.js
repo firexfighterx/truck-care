417585301e5163a9cd323dd0cffc221f9171a1c6
Brandon Allen <firexfighterx@gmail.com>
2016-09-06 21:40:15 -0500

    Removing About route and updated to have Truck Detail defined route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
-import Home from './components/home/Home';
+import TruckDetail from './components/TruckDetail/TruckDetail';
 import About from './components/about/About';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="about" component={About} />
+    <Route path="/TruckDetail/:id" component={TruckDetail} />
   </Route>
 );
