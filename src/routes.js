3f553142d049c5541254fd5cbfbdce96e23b3878
Brandon Allen <firexfighterx@gmail.com>
2016-09-14 19:02:10 -0500

    Removed old About Route as it is no longer needed

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
-import App from './components/App';
-import TruckDetail from './components/TruckDetail/TruckDetail';
-import About from './components/about/About';
+import App from './components/App';//eslint-disable-line import/no-named-as-default
+import TruckDetail from './components/TruckDetail/TruckDetail';//eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={App}>
