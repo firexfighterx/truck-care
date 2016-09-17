5f5dcf9f6bb09ea3ef72e2b49b27511c4bfa3513
Brandon Allen <firexfighterx@gmail.com>
2016-09-17 16:59:26 -0500

    Adding App.js tests for GlobalNotification component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
+/*eslint-disable react/jsx-no-bind */
 import React, {Component,PropTypes} from 'react';
 import Sidebar from './common/Sidebar';
+import GlobalNotification from './common/GlobalNotification';
+import * as GlobalErrorActions from '../actions/GlobalErrorActions';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {browserHistory} from 'react-router';
 
 export class App extends Component {
+
+constructor(props){
+  super(props);
+}
+
+_handleCloseGlobalNotification(){}
+
     render() {
         return (
             <div>
@@ -12,6 +22,7 @@ export class App extends Component {
                     <Sidebar trucks={this.props.trucks}/>
                 </div>
                 <div className="main-content-container">
+                  <GlobalNotification onDismiss={this._handleCloseGlobalNotification.bind(this)} notification={this.props.globalNotification} />
                   {this.props.children}
                 </div>
             </div>
@@ -21,7 +32,8 @@ export class App extends Component {
 
 App.propTypes = {
     children: PropTypes.object.isRequired,
-    trucks: PropTypes.arrayOf(PropTypes.object)
+    trucks: PropTypes.arrayOf(PropTypes.object),
+    globalNotification: PropTypes.object.isRequired
 };
 
 
@@ -29,6 +41,12 @@ function mapStateToProps(state, ownProps) {
     return {
         trucks: state.trucks
     };
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    actions: bindActionCreators()
+  };
 }
 
 export default connect(mapStateToProps)(App);
--- src/tests/components/AppTests.js
+++ src/tests/components/AppTests.js
@@ -2,10 +2,9 @@ import sinon from 'sinon';
 import assert from 'assert';
 import React from 'react';
 import Sidebar from '../../components/common/Sidebar';
+import GlobalNotification from '../../components/common/GlobalNotification';
 import * as ShallowTestUtils from 'react-shallow-testutils';
-import {
-    App
-} from '../../components/App';
+import {App} from '../../components/App';
 
 describe('App', () => {
     let sandbox;
@@ -20,7 +19,7 @@ describe('App', () => {
 
     describe('render', () => {
         it('renders a sidebar and children', () => {
-            let children = < ul > < /ul>;
+            let children = <ul></ul>;
             let trucks = [];
             let props = {
                 children,
@@ -31,6 +30,29 @@ describe('App', () => {
             let components = ShallowTestUtils.findAllWithType(result, Sidebar);
             assert.strictEqual(components.length, 1, 'one sidebar element was rendered');
             assert.strictEqual(components[0].props.trucks, trucks, 'trucks prop was passed to sidebar');
+        });
+
+        it('renders a GlobalNotification when an error is present', () => {
+          let children = <ul></ul>;
+          let trucks = [];
+          let globalNotification = {
+            type: 'danger',
+            message: 'Message'
+          };
+          let boundFunction = () => {};
+          let props = {
+              children,
+              trucks,
+              globalNotification
+          };
+          let testObject = new App(props);
+          let boundHandleCloseGlobalNotification = sandbox.stub(testObject._handleCloseGlobalNotification, 'bind').returns(boundFunction);
+          let result = testObject.render();
+          let components = ShallowTestUtils.findAllWithType(result, GlobalNotification);
+          assert.strictEqual(components.length, 1, 'one GlobalNotification element was rendered');
+          assert.strictEqual(components[0].props.notification, globalNotification, 'globalNotification prop was passed to GlobalNotification');
+          assert.strictEqual(components[0].props.onDismiss, boundFunction, 'bound handle close global notification passed to GlobalNotification');
+          assert(boundHandleCloseGlobalNotification.calledOnce, 'called bound handleGetNotification method');
         });
     });
 });
