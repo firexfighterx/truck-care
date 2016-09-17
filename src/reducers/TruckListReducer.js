19d0572819b271252234e1e52debff2411931e1e
Brandon Allen <firexfighterx@gmail.com>
2016-09-17 14:30:29 -0500

    Renaming TruckCareReducer to TruckListReducer

--- src/reducers/TruckListReducer.js
+++ src/reducers/TruckListReducer.js
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function TruckCareReducer(state = initialState.trucks, action) {
+export default function TruckListReducer(state = initialState.trucks, action) {
     switch (action.type) {
         case types.GET_ALL_TRUCKS_SUCCESS:
             return action.trucks;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,8 @@
-import {
-    combineReducers
-} from 'redux';
-import {
-    routerReducer
-} from 'react-router-redux';
-import trucks from './TruckCareReducer';
+import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
+import trucks from './TruckListReducer';
 import ajaxCallsInProgress from './ajaxStatusReducer';
 
-const rootReducer = combineReducers({
-    ajaxCallsInProgress,
-    trucks,
-    routing: routerReducer
-});
+const rootReducer = combineReducers({ajaxCallsInProgress, trucks, routing: routerReducer});
 
 export default rootReducer;
