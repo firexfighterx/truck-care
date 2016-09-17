4dd64dc3df7f8604881e63348d8d19df5df1439b
Brandon Allen <firexfighterx@gmail.com>
2016-09-17 16:01:52 -0500

    Add test for updating store with global error

--- src/tests/store/storeTests.js
+++ src/tests/store/storeTests.js
@@ -3,6 +3,7 @@ import {createStore} from 'redux';
 import rootReducer from '../../reducers';
 import initialState from '../../reducers/initialState';
 import * as truckCareActions from '../../actions/TruckListActions';
+import * as globalErrorActions from '../../actions/GlobalErrorActions';
 
 describe('Store', function() {
     it('should update store with dispatched trucks', () => {
@@ -19,5 +20,21 @@ describe('Store', function() {
 
         let actual = store.getState().trucks;
         assert.deepEqual(actual, trucks, 'trucks was set on the state store');
+    });
+
+    it('should update store with dispatched global error', () => {
+        let store = createStore(rootReducer, initialState);
+
+        let globalErrorNotification = {
+            type: 'danger',
+            message: 'Yo this is a message'
+        };
+
+        let action = globalErrorActions.setGlobalError(globalErrorNotification);
+        store.dispatch(action);
+
+        let actual = store.getState().globalErrorNotification;
+
+        assert.deepEqual(actual, globalErrorNotification, 'global error notification was set on the store');
     });
 });
