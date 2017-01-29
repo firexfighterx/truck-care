e2503cacebae53e809aa654c5e8f5dbd5b9698dc
Brandon Allen <brandon.allen@asynchrony.com>
2017-01-29 14:04:43 -0600

    Added setGlobalMessage Success method to Global Message actions

--- src/actions/GlobalMessageActions.js
+++ src/actions/GlobalMessageActions.js
@@ -3,4 +3,7 @@ import * as actions from './Actions';
 export function setGlobalError(globalMessage) {
     return { type: actions.SET_GLOBAL_ERROR, globalMessage };
 }
+
+export function setGlobalSuccess(globalMessage) {
+    return { type: actions.SET_GLOBAL_SUCCESS, globalMessage };
 }
--- src/tests/actions/GlobalMessageActionsTests.js
+++ src/tests/actions/GlobalMessageActionsTests.js
@@ -18,6 +18,21 @@ describe('GlobalErrorActions', () => {
             assert.deepEqual(actual, expected, 'SET_GLOBAL_ERROR was dispatched with the globalMessage');
         });
     });
+
+    describe('setGlobalSuccess', () => {
+        it('returns SET_GLOBAL_SUCCESS action to dispatch', () => {
+            let globalMessage = {
+                type: 'success',
+                message: 'There is a snake in my boot'
+            };
+            let expected = {
+                type: 'SET_GLOBAL_SUCCESS',
+                globalMessage
+            };
+
+            let actual = GlobalErrorActions.setGlobalSuccess(globalMessage);
+
+            assert.deepEqual(actual, expected, 'SET_SUCCESS_ERROR was dispatched with the globalMessage');
         });
     });
 });
